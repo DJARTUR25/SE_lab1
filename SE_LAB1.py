b6651436db19db77a46cb23b51350aef05ec4fd4
DJARTUR25 <djartur25@gmail.com>
2024-10-27 19:39:55 +0300

    Added new functions
    -> Added function that calculating eigenvalues and eigenvectors
    -> Added function that defines the type
    -> Added function that print complex numbers

--- SE_LAB1.py
+++ SE_LAB1.py
@@ -2,16 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
+
 def f1(alpha, beta):    # function equal system of differenctional 
   def rhs(t, X):
     x,y = X
-    return [y, -2*alpha - x*beta]
+    return [y, -(x**3) - (x**2) + 2*x]
   return rhs
 
 
 def jacobian(alpha, beta):    # function of matrix of Jacobi
   return np.array([[0,1], [-alpha, -2*beta]])
 
+
+def calc_eigvalues (alpha, beta):     # function that calculating eigenvalues and eigenvectors 
+  vals, vectors = np.linalg.eig(jacobian(alpha,beta))
+  return vals, vectors
+
+def get_type(vals):     # determines what type of equilibrium a given state has
+  real = vals.real
+  imag = vals.imag
+  
+  if np.all(real < 0) and np.all(imag == 0):    # if real parts of eigenvalues are less than 0 and imagine parts do not exist => stable node
+    return "Stable node"
+  if np.all(real > 0) and np.all(imag == 0):    # if real parts of eigenvalues are more than 0 and imagine parts do not exist => unstable node
+    return "Unstable node"
+  if np.any (real < 0) and np.any (real > 0) and np.all(imag == 0):   # if real parts of a different signs and imagine parts do not exist => saddle
+    return "Saddle"
+  if np.all(real < 0) and np.all(imag != 0):    # if real parts of eigenvalues are less than 0 and imagine parts exists => stable focus
+    return "Stable focus"
+  if np.all(real > 0) and np.all(imag != 0):    # if real parts of eigenvalues are more than 0 and imagine parts exists => unstable focus
+    return "Unstable foсus"
+  if np.all(real == 0) and np.all(imag != 0):   # if eigenvalues are purely imaginary => center (or difficult focus, more research is needed)
+    return "Center"
+
+
+def print_complex_numbers(num):
+  if np.iscomplex(num):
+    if num.imag >= 0: 
+      sign = '+'
+    else:
+      sign = '-'
+    return f"{num.real::2f} {sign} {abs(num.imag)::2f}i"
+  return f"{num::2f}"
+  
 
 def eq_quiver(rhs, limits, N=16):   # function creates the limits of linear limits
   xlims, ylims = limits
@@ -26,7 +59,9 @@ def eq_quiver(rhs, limits, N=16):   # function creates the limits of linear limi
       u, v = vfield
       U[i][j] = u
       V[i][j] = v
+
   return xs, ys, U, V
+
 
 def plotonPlane(rhs, limits):   # function draws the phase plate
   plt.close()
@@ -35,6 +70,7 @@ def plotonPlane(rhs, limits):   # function draws the phase plate
   plt.ylim(ylims[0], ylims[1])
   xs, ys, U, V = eq_quiver(rhs, limits)
   plt.quiver(xs, ys, U, V, alpha=0.8)
+
 
 alpha = 1.0     # main, makes the constants
 beta = 1.0
